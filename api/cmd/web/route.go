a8af2bc9609631a575e044cdfd257807b082eaf8
lokicodess <lokesh3721@gmail.com>
2025-12-27 18:49:37 +0530

    add prometheus client

--- api/cmd/web/route.go
+++ api/cmd/web/route.go
@@ -1,11 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func (app *app) routes() http.Handler {
 
 	mux := http.NewServeMux()
-
+	mux.Handle("GET /metrics", promhttp.Handler())
 	mux.HandleFunc("GET /healthcheck", app.healthCheck)
 	mux.HandleFunc("GET /{short_code}", app.GetUrl)
 	mux.HandleFunc("POST /shorten", app.HandleShorten)
